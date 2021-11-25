80b5071280594218d55ba4a686bcb521346d5411
wangxy <wxy12125@vip.qq.com>
2021-11-25 22:40:06 +0800

    五角星

---
+++ learn.go/00.hello/exercise/hello_test.go
@@ -0,0 +1,35 @@
+package exercise
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExercise(t *testing.T) {
+	fmt.Println("hello world")
+	var touHigh int = 6
+	var jianHigh int = 25
+	var kuang int = 50
+	for i := 0; i <= touHigh+jianHigh; i++ {
+		for j := 0; j <= kuang; j++ {
+			if i <= touHigh {
+				if j > (kuang/2+1)+1-i && j <= (kuang/2+1)-1+i {
+					fmt.Print("*")
+				} else {
+					fmt.Print("-")
+				}
+			}
+
+			if i > touHigh && i < jianHigh {
+				if j >= (kuang/2+1)+2-i && j <= kuang-3*(i-touHigh) {
+					fmt.Print("*")
+				} else if j < (kuang/2+1)-3+i && j >= 3*(i-touHigh) {
+					fmt.Print("*")
+				} else {
+					fmt.Print("-")
+				}
+			}
+		}
+		fmt.Println("")
+	}
+}
