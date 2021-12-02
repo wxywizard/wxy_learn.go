aee8a75a898c240d0cd3cb79a1f1522c60288cca
wangxy@sit.com.cn <Sailinggp@#$123>
2021-12-02 13:46:26 +0800

    体脂计算器v1.0

--- learn.go/homework/bmi/main.go
+++ learn.go/homework/bmi/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -19,20 +20,33 @@ var (
 )
 
 func main() {
+	for {
+		s := process()
+		fmt.Println("the physique is " + s)
+		fmt.Println("whether to continue？(Y/N)")
+		isContinue := read("isContinue")
+		upper := strings.ToUpper(strings.TrimSpace(isContinue))
+		if "N" == upper {
+			os.Exit(-1)
+		}
+	}
+}
+
+func process() string {
 	var sex int
 	fmt.Println("Please enter sex: ")
 	s := read("sex")
-	switch s {
+	switch strings.TrimSpace(s) {
 	case "男":
-		sex = 1
+		sex = man
 	case "女":
-		sex = 0
+		sex = woman
 	default:
 		fmt.Println("the input is invalid")
 	}
 	fmt.Println("Please enter weight(kg): ")
 	w := read("weight")
-	wFloat, wErr := strconv.ParseFloat(w, 64)
+	wFloat, wErr := strconv.ParseFloat(strings.TrimSpace(w), 64)
 	if wErr == nil {
 		weight = wFloat
 	} else {
@@ -40,7 +54,7 @@ func main() {
 	}
 	fmt.Println("Please enter height(m): ")
 	h := read("height")
-	hFloat, hErr := strconv.ParseFloat(h, 64)
+	hFloat, hErr := strconv.ParseFloat(strings.TrimSpace(h), 64)
 	if hErr == nil {
 		height = hFloat
 	} else {
@@ -48,23 +62,29 @@ func main() {
 	}
 	fmt.Println("Please enter age: ")
 	a := read("age")
-	aInt, aErr := strconv.ParseInt(a, 10, 64)
+	aInt, aErr := strconv.ParseInt(strings.TrimSpace(a), 10, 64)
 	if aErr == nil {
 		age = aInt
 	} else {
 		fmt.Println("the input is invalid")
 	}
 	//计算体脂
+	var physiqueResult string
 	physique := calculationPhysique(weight, height, age, sex)
-	fmt.Sprintf("%.2f", physique)
-	//printf, wErr := fmt.Printf("%%", res)
+	if sex == man {
+		physiqueResult = switchMan(physique, age)
+	}
+	if sex == woman {
+		physiqueResult = switchWoman(physique, age)
+	}
+	return physiqueResult
 }
 
 func read(msg string) string {
 	reader := bufio.NewReader(os.Stdin)
 	readString, err := reader.ReadString('\n')
 	if err == nil {
-		fmt.Printf("The input "+msg+"was: %s\n", readString)
+		fmt.Printf("The input "+msg+" was: %s\n", readString)
 	}
 	return readString
 }
@@ -76,5 +96,123 @@ BMI=体重（公斤）÷（身高×身高）（米）
 func calculationPhysique(w float64, h float64, a int64, s int) float64 {
 	bmi := w / (h * h)
 	result := 1.2*bmi + 0.23*(float64)(a) - 5.4 - 10.8*(float64)(s)
+	return result
+}
+
+func switchMan(physique float64, age int64) string {
+	var result string
+	if age >= 18 && age <= 39 {
+		if physique >= 0.00 && physique <= 0.10 {
+			result = "偏瘦"
+		}
+		if physique > 0.10 && physique <= 0.16 {
+			result = "标注"
+		}
+		if physique > 0.16 && physique <= 0.21 {
+			result = "偏重"
+		}
+		if physique > 0.21 && physique <= 0.26 {
+			result = "肥胖"
+		}
+		if physique > 0.26 {
+			result = "严重肥胖"
+		}
+	}
+
+	if age >= 40 && age <= 59 {
+		if physique >= 0.00 && physique <= 0.11 {
+			result = "偏瘦"
+		}
+		if physique > 0.11 && physique <= 0.17 {
+			result = "标注"
+		}
+		if physique > 0.17 && physique <= 0.22 {
+			result = "偏重"
+		}
+		if physique > 0.22 && physique <= 0.27 {
+			result = "肥胖"
+		}
+		if physique > 0.27 {
+			result = "严重肥胖"
+		}
+	}
+
+	if age >= 60 {
+		if physique >= 0.00 && physique <= 0.13 {
+			result = "偏瘦"
+		}
+		if physique > 0.13 && physique <= 0.19 {
+			result = "标注"
+		}
+		if physique > 0.19 && physique <= 0.24 {
+			result = "偏重"
+		}
+		if physique > 0.24 && physique <= 0.29 {
+			result = "肥胖"
+		}
+		if physique > 0.29 {
+			result = "严重肥胖"
+		}
+	}
+
+	return result
+}
+
+func switchWoman(physique float64, age int64) string {
+	var result string
+	if age >= 18 && age <= 39 {
+		if physique >= 0.00 && physique <= 0.19 {
+			result = "偏瘦"
+		}
+		if physique > 0.19 && physique <= 0.27 {
+			result = "标注"
+		}
+		if physique > 0.27 && physique <= 0.34 {
+			result = "偏重"
+		}
+		if physique > 0.34 && physique <= 0.39 {
+			result = "肥胖"
+		}
+		if physique > 0.39 {
+			result = "严重肥胖"
+		}
+	}
+
+	if age >= 40 && age <= 59 {
+		if physique >= 0.00 && physique <= 0.21 {
+			result = "偏瘦"
+		}
+		if physique > 0.21 && physique <= 0.28 {
+			result = "标注"
+		}
+		if physique > 0.28 && physique <= 0.35 {
+			result = "偏重"
+		}
+		if physique > 0.35 && physique <= 0.40 {
+			result = "肥胖"
+		}
+		if physique > 0.40 {
+			result = "严重肥胖"
+		}
+	}
+
+	if age >= 60 {
+		if physique >= 0.00 && physique <= 0.22 {
+			result = "偏瘦"
+		}
+		if physique > 0.22 && physique <= 0.29 {
+			result = "标注"
+		}
+		if physique > 0.29 && physique <= 0.36 {
+			result = "偏重"
+		}
+		if physique > 0.36 && physique <= 0.41 {
+			result = "肥胖"
+		}
+		if physique > 0.41 {
+			result = "严重肥胖"
+		}
+	}
+
 	return result
 }
